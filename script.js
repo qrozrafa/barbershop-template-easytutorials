c93ddf8653b0e0bf75573071b6eaf3ee61bf24a4
qrozrafa <rafael_queirozs@hotmail.com>
2021-07-28 22:23:36 -0400

    active and remove sideNav onclick

--- script.js
+++ script.js
@@ -14,6 +14,17 @@ menuBtn.onclick = function(){
     }
 }
 
+sideNav.onclick = function(){
+    if(sideNav.style.right == "-156px"){
+        sideNav.style.right = "0";
+        menu.src ="./images/close.png";
+    }
+    else{
+        sideNav.style.right = "-156px";
+        menu.src="images/menu.png";
+    }
+}
+
 var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 1000,
 	speedAsDuration: true
